6908ab3183546d8d35a1b0a8ac389fdb81b19641
FrenchPlatypus <astrucjoa@gmail.com>
2022-01-14 17:37:31 +0100

    Go fish

--- Assets/Scripts/Minigames/Joel/GoFish.cs
+++ Assets/Scripts/Minigames/Joel/GoFish.cs
@@ -1,21 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GoFish : MonoBehaviour
 {
+    [SerializeField] private StoryModeManager manager;
+    [SerializeField] private float timer;
 
-    public bool catchFish;
+    [SerializeField] private Transform fishParent;
+    [SerializeField] private Animator marioParent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        manager = GameObject.Find("StoryModeManager").GetComponent<StoryModeManager>();
+        manager.minigameTimer = timer;
+        manager.maxTimer = timer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && fishParent.GetComponent<Button>().enabled)
+            MinigameWon();
+    }
+
+    public void MinigameWon()
+    {
+        fishParent.gameObject.SetActive(false);
+        marioParent.enabled = true;
+        manager.minigameWon = true;
+
+        if (manager.minigameTimer > 2)
+        {
+            manager.minigameTimer = 2;
+        }
     }
 }
